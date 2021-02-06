d444093de2e0e8cd3f942bc4079ef88a5d1b154b
Jose Ibanez <jose.ibanez@vodafone.com>
2021-02-06 22:16:42 +0100

    whell

--- p01/deploy/setup-github-python.sh
+++ p01/deploy/setup-github-python.sh
@@ -4,7 +4,12 @@ pwd
 export
 ls -l
 
+#Prepare pip
 python -m pip install --upgrade pip
+pip install --upgrade setuptools wheel twine
+pip install --upgrade --ignore-installed PyYAML
+
+#Install requirements
 pip install -r requirements.txt
 pip install -r requirements-test.txt
 pip install -e ..
